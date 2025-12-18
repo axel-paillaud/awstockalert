e736551ed2b7de679b9f70001e47d8cf3ecfdef2
Axel Paillaud <contact@axelweb.fr>
2025-12-18 14:18:20 +0100

    first commit, fetch base module template

---
+++ awmodulebase.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * 2007-2024 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    Axelweb <contact@axelweb.fr>
+ * @copyright 2007-2024 Axelweb
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of Axelweb
+ */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+class AwModuleBase extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'awmodulebase';
+        $this->tab = 'administration';
+        $this->version = '1.0.0';
+        $this->author = 'Axelweb';
+        $this->need_instance = 1;
+
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->trans('Axelweb - Module Base', [], 'Modules.Awmodulebase.Admin');
+        $this->description = $this->trans('Base template module for PrestaShop development', [], 'Modules.Awmodulebase.Admin');
+
+        $this->confirmUninstall = $this->trans('Are you sure you want to uninstall this module?', [], 'Modules.Awmodulebase.Admin');
+
+        $this->ps_versions_compliancy = [
+            'min' => '8.0',
+            'max' => _PS_VERSION_,
+        ];
+    }
+
+    public function isUsingNewTranslationSystem()
+    {
+        return true;
+    }
+
+    public function install(): bool
+    {
+        if (Shop::isFeatureActive()) {
+            Shop::setContext(Shop::CONTEXT_ALL);
+        }
+
+        $installed = parent::install()
+            && $this->installDb()
+            && $this->registerHook('actionFrontControllerSetMedia')
+            && Configuration::updateValue('AWMODULEBASE_SAMPLE_CONFIG', '');
+
+        // Prevent 'Unable to generate a URL for the named route [...]' error,
+        // clear Symfony cache
+        if ($installed) {
+            Tools::clearSf2Cache();
+        }
+
+        return $installed;
+    }
+
+    public function uninstall(): bool
+    {
+        return parent::uninstall()
+            && Configuration::deleteByName('AWMODULEBASE_SAMPLE_CONFIG')
+            && $this->uninstallDb();
+    }
+
+    protected function installDb(): bool
+    {
+        $file = __DIR__ . '/sql/install.php';
+
+        return is_file($file) ? (bool) require $file : false;
+    }
+
+    protected function uninstallDb(): bool
+    {
+        $file = __DIR__ . '/sql/uninstall.php';
+
+        return is_file($file) ? (bool) require $file : false;
+    }
+
+    /**
+     * Redirect to the module symfony configuration page
+     *
+     * @return void
+     */
+    public function getContent(): void
+    {
+        $route = $this->get('router')->generate('awmodulebase_form_configuration');
+        Tools::redirectAdmin($route);
+    }
+
+    /**
+     * Hook to register CSS and JS on front-office pages
+     */
+    public function hookActionFrontControllerSetMedia()
+    {
+        $this->context->controller->registerStylesheet(
+            'module-awmodulebase-style',
+            'modules/' . $this->name . '/views/css/awmodulebase.css',
+            [
+                'media' => 'all',
+                'priority' => 200,
+            ]
+        );
+
+        $this->context->controller->registerJavascript(
+            'module-awmodulebase-script',
+            'modules/' . $this->name . '/views/js/awmodulebase.js',
+            [
+                'position' => 'bottom',
+                'priority' => 200,
+            ]
+        );
+    }
+}
---
+++ src/Form/GeneralFormType.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    Axelweb <contact@axelweb.fr>
+ * @copyright 2007-2024 Axelweb
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace Axelweb\AwModuleBase\Form;
+
+use PrestaShopBundle\Form\Admin\Type\TranslatorAwareType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class GeneralFormType extends TranslatorAwareType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('sample_config', TextType::class, [
+                'label' => $this->trans('Sample configuration', 'Modules.Awmodulebase.Admin'),
+                'help' => $this->trans('Example configuration field', 'Modules.Awmodulebase.Admin'),
+                'required' => false,
+                'constraints' => [
+                    new Assert\Length(['max' => 255]),
+                ],
+                'attr' => [
+                    'placeholder' => 'Example value',
+                    'autocomplete' => 'off',
+                ],
+            ]);
+    }
+}
